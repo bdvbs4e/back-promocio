6f92ab9f620cf68cd81113e5e00d9ab93f675c0b
bdvbs4e <danielfarid63@gmail.com>
2024-10-27 17:55:58 -0500

    cambio#182812

--- routes/userRoutes.routes.js
+++ routes/userRoutes.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {validateCredentials, Signup} = require('./controllers/user.js');
 
 router.post('/login', validateCredentials);
-router.post('/Signup', Signup);
+router.post('/signup', Signup);
 
 module.exports = router;
