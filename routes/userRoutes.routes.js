c98db4338cfa3c4f74987ce329c0352694c444b6
bdvbs4e <danielfarid63@gmail.com>
2024-10-27 16:49:33 -0500

    dios mio que esta sea

--- routes/userRoutes.routes.js
+++ routes/userRoutes.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {validateCredentials, Signup} = require('./controllers/users.js');
 
 router.post('/login', validateCredentials);
-router.post('/signup', Signup);
+router.post('/Signup', Signup);
 
 module.exports = router;
